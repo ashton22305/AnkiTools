942fa162e3b73d849616bb9264de6910b79a963b
Ashton South <aasouth223+github@gmail.com>
2023-10-28 20:53:32 -0400

    Create base classes

--- AnkiTools.js
+++ AnkiTools.js
@@ -1,2 +1,17 @@
-console.log("Hello, world!");
-console.log("This is a test of github");
+class Card {
+    constructor(front, back) {
+        this.front = front;
+        this.back = back;
+    }
+}
+
+class Deck {
+    constructor(name, cards) {
+        this.name = name;
+        this.cards = cards;
+    }
+
+    genCSV() {
+        pass
+    }
+}
